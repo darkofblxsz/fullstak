586f6025eaff98b81847fe07c8b7c2e06fa72632
Tozuora <116980841+Tozuora@users.noreply.github.com>
2026-06-12 12:16:52 -0400

    Delete ms-categoria directory
    a

--- ms-categoria/src/main/java/com/example/ms_categoria/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_categoria.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
