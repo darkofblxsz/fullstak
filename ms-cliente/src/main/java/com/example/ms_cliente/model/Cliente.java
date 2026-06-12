32baa47bb5bbc148b20c68fbe67f3851ac96f0f2
Tozuora <116980841+Tozuora@users.noreply.github.com>
2026-06-12 12:37:15 -0400

    Add files via upload

---
+++ ms-cliente/src/main/java/com/example/ms_cliente/model/Cliente.java
@@ -0,0 +1,37 @@
+package com.example.ms_cliente.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    @Column(unique=true)
+    private String correo;
+
+    private String telefono;
+
+}
