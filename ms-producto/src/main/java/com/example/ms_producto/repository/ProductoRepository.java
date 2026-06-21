7533bc18bc07654a96369b818d220377bc243f9d
Tozuora <116980841+Tozuora@users.noreply.github.com>
2026-06-21 19:54:32 -0400

    Delete ms-producto directory

--- ms-producto/src/main/java/com/example/ms_producto/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_producto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ms_producto.producto.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto,Long>{
-}
